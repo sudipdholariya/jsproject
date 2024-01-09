d23da9733ff0c64ab581e1555ccb22f0ebbb284f
PD <zeroprojecthub@gmail.com>
2024-01-09 17:06:24 +0530

    First file added

---
+++ first.js
@@ -0,0 +1,74 @@
+
+
+// const id=4545;
+
+// account=5656;
+let accountname=343434343n
+// let balance=33.454
+// balance=99
+// balance=true
+// let acno=null;
+
+// String
+//Number
+// boolean
+//object
+// bigint
+//undefine
+// Symbol
+
+// {
+//     var accountid=232
+//     let accountnumber=34343;
+//     console.log(accountnumber)
+//     console.log(accountid)
+// }
+
+
+// console.log(accountnumber)
+
+// console.log(typeof accountname,accountname)
+// console.log(2+3)
+// console.log(2-3)
+// console.log(2*3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let v=parseInt(4454.565);
+// let val="44";
+// // val=true
+// // let another = Number(val)
+// let val1=45;
+// val1 = 0
+// // let another = String(val1)
+// let val2 = null
+// let another=Boolean(val2);
+// console.log(typeof another,another);
+
+// let num1 = 5;
+// let num2 = "55";
+// let str="rahul raj";
+// let ans;
+
+// // ans=str.charAt(0);
+// ans=str.length
+// ans=str.includes('raj')
+// let url = "https:/www.fkdf.hash%20refrr"
+
+// ans=url.replace('%20','-');
+// ans = str.indexOf('h')
+// console.log(typeof ans,ans);
+
+// console.log(Math.floor(2.343));
+// console.log(Math.ceil(454.76));
+
+// console.log(Math.round(34.63));
+// console.log(Math.pow(2,5));
+
+// console.log(2**3);
+// console.log(Math.min(22,33,44,5,6));
+// console.log(Math.max(454,56,77));
+
+// console.log(Math.floor(Math.random()*100+1));
+
+
