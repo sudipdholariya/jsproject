9f50ad5a7bd659c76640dbc86ca29739edcb4570
sudeep <sudipdholariya8@gmail.com>
2024-01-18 17:46:55 +0530

    Higher order method

---
+++ loop.js
@@ -0,0 +1,120 @@
+
+// for (let index = 0; index < nums.length; index++) {
+//     const element = nums[index];
+//      console.log(element);   
+// }
+
+
+
+// for(let num of nums){
+//     console.log(num);
+// }
+
+// for(let fruit of fruits){
+//     console.log(fruit);
+// }
+
+// for(let ob of person){
+//     console.log(ob);
+// }
+
+
+// for(let item in person){
+//     console.log(item,person[item]);
+// }
+
+// for(let fruit in fruits){
+//     console.log(fruits[fruit]);
+// }
+// let fruits= ["apple","orange","banana","pinepale"]
+// let nums = [22,33,4,5,56]
+
+
+// let person = {
+//         name:"sudip",
+//         age:23,
+//         email:"creative@gmail.com"
+// }
+
+
+// let newarr=nums.map((num)=>{
+//     return num*2;
+// })
+
+    // let oldarr = nums.filter((item)=>{
+    //         return item>10
+    // })
+
+    // let total =  nums.reduce((acc,sum)=>{
+    //         return acc+sum
+    // },0)
+
+// console.log(total);
+// console.log(nums);
+
+
+let product = [
+    {
+        name:"rahul",
+        age:22,
+        email:"creative@gmail.com"
+    },{
+        
+        name:"kavan",
+        age:26,
+        email:"creative22@gmail.com"
+    },{
+        
+        name:"kiran",
+        age:55,
+        email:"creative33@gmail.com"
+    }
+]
+
+product.forEach((item)=>{
+    // console.log(item.name);
+   
+})
+
+
+// console.log(Object.values(product));
+// let map = new Map()
+
+// map.set("a",1)
+// map.set("b",2)
+// map.set("a",99);
+// map.set("three",33)
+
+// console.log(map);
+
+// map.forEach((item,val)=>{
+//     console.log(item,val);
+// })
+
+
+
+// fruits.forEach((item)=>{
+//     console.log(item);
+// })
+// function flist(item){
+//     console.log(item);
+// }
+
+// fruits.forEach(flist)
+
+// fruits.forEach(function(item){
+//         console.log(item);
+// })
+
+// nums.forEach((num)=>{
+//         console.log(num);
+// })
+
+// person.forEach(element => {
+//     console.log(element);
+// });
+
+
+
+
+
