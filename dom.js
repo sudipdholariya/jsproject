8bd511fac0c70ae499efb62eba3be2128732e9c2
sudeep <sudipdholariya8@gmail.com>
2024-01-24 17:14:07 +0530

    file updated

--- dom.js
+++ dom.js
@@ -1,3 +1,29 @@
+
+
+// Global window object
+console.log(window);
+// The document object is part of the window object
+console.dir(window.document);
+// We can access DOM elements directly with properties
+console.log(document.body);
+console.log(document.links[0]);
+
+// we can set properties of elements 
+//document.body.innerHTML='<h1>Hello from Body</h1>';
+
+// The document object has a ton of methods. One is `document.write()`, which will write something to the document
+// document.open();
+// document.write('Hello from JS');
+// document.close();
+
+// We also have methods to select elements more directly
+
+//document.getElementById('main').innerHTML='<h1> Helloo </h1>';
+
+// document.querySelector('#main h1').innerText='how are Yoy DOM??';
+//document.querySelector('#main h1').textContent='Say somthing Google';
+
+
 // <!DOCTYPE html>
 /*<html lang="en">
 <head>
@@ -78,8 +104,8 @@
     div.setAttribute("title", "generated title")
     div.style.backgroundColor = "green"
     div.style.padding = "12px"
-    // div.innerText = "Chai aur code"
-    const addText = document.createTextNode("Chai aur code")
+    // div.innerText = "code"
+    const addText = document.createTextNode("code")
     div.appendChild(addText)
 
     document.body.appendChild(div)
